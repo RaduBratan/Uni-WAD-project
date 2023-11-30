ab8f6c4a985185fee4f06c83fcdbeb3fa9401399
RaduBratan <bratanradu7@gmail.com>
2023-11-30 10:51:11 +0200

    Initial project setup

---
+++ NET-Restaurant-API/NET-Restaurant-API/Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using NET_Restaurant_API;
+using NET_Restaurant_API.Data;
+using NET_Restaurant_API.Helper;
+using NET_Restaurant_API.Helper.Extensions;
+using NET_Restaurant_API.Helper.Middleware;
+using NET_Restaurant_API.Models;
+using NET_Restaurant_API.Repositories.DatabaseRepository;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+}); 
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+builder.Services.AddRepositories();
+builder.Services.AddServices();
+builder.Services.AddUtils();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+
+var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseMiddleware<JwtMiddleware>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
