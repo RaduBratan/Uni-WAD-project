c9fb82fa28bf38bdaa50a9014feb4027e8167bdb
RaduBratan <bratanradu7@gmail.com>
2023-12-20 17:24:22 +0200

    Update some components

--- NET-Restaurant-Frontend/src/components/dropdown/Dropdown.jsx
+++ NET-Restaurant-Frontend/src/components/dropdown/Dropdown.jsx
@@ -1,5 +1,32 @@
-const Dropdown = () => {
-    return <></>;
+import DropdownItem from "../dropdownItem/DropdownItem";
+import "./Dropdown.css";
+
+const Dropdown = ({services,
+                   setMethodName,
+                   setBodyInputs,
+                   setParamInputs, 
+                   setBodyInputsData, 
+                   setParamInputsData, 
+                   setMethod, 
+                   setUrl }) => {
+    return (
+        <div className="Dropdown">
+            {services.map(service => {
+                return <DropdownItem
+                    key={service.id}
+                    name={service.name}
+                    actions={service.actions}
+                    setMethodName={setMethodName}
+                    setBodyInputs={setBodyInputs}
+                    setParamInputs={setParamInputs}
+                    setBodyInputsData={setBodyInputsData}
+                    setParamInputsData={setParamInputsData}
+                    setMethod={setMethod}
+                    setUrl={setUrl}
+                />
+            })}
+        </div>
+    )
 };
 
 export default Dropdown;
--- NET-Restaurant-Frontend/src/components/sidebar/Sidebar.jsx
+++ NET-Restaurant-Frontend/src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,30 @@
 import Dropdown from "../dropdown/Dropdown";
 import SidebarHeader from "../sidebarHeader/SidebarHeader";
 
-const Sidebar = ({username, services}) => {
+const Sidebar = (
+                {username,
+                 email,
+                 services,
+                 setMethodName,
+                 setBodyInputs,
+                 setParamInputs,
+                 setBodyInputsData,
+                 setParamInputsData,
+                 setMethod,
+                 setUrl}) => {
     return (
         <div className="Sidebar">
-            <SidebarHeader username={username}/>
-            <Dropdown services={services}/>
+            <SidebarHeader username={username} email={email}/>
+            <Dropdown 
+                services={services}
+                setMethodName={setMethodName}
+                setBodyInputs={setBodyInputs} 
+                setParamInputs={setParamInputs}
+                setBodyInputsData={setBodyInputsData}
+                setParamInputsData={setParamInputsData}
+                setMethod={setMethod} 
+                setUrl={setUrl} 
+            />
         </div>
     );
 };
